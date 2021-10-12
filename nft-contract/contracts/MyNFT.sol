47fac9552bab71f96c077c9844841033dc169a7d
Joshua Oladeji <dejijolaoluwa@gmail.com>
2021-10-12 20:23:18 +0100

    added methods to the contract

--- nft-contract/contracts/MyNFT.sol
+++ nft-contract/contracts/MyNFT.sol
@@ -8,52 +8,190 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 contract MyNFT is ERC721URIStorage, Ownable {
+    
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
     
+    
+    
     // -> contract variables
-    address payable public contractOwner = payable(0xAC14513E6B0e6D403592CbB21E0E044726CcF3E4);
+    bool public presale = true;
+    bool public paused = false;
+    uint256 public maxSupply = 7777;
+    uint256 public cost = 0.07 ether;
+    uint256 public airdropTokenId = 0;
+    address[] whitelistedAddresses;
+    
 
+
+    // -> contract constructor
     constructor() public ERC721("MyNFT", "NFT") {}
 
-    function mintNFT(address recipient, string memory tokenURI)
+
+
+    // -> contract methods
+    function mintNFT(address _recipient, string memory _tokenURI)
         public payable
         returns (uint256)
     {
-        require(msg.value >= 20000000000000000, "Not enough ETH sent; check price!");
+        if (presale)
+        {
+            // check if the address is whitelisted
+            require(isAddressWhitelisted(_recipient), 'Only whitelisted addresses can mint during presale');
+        }
+        
+        require(!paused, 'Minting is paused');
+        require(msg.value >= cost, 'Not enough ETH sent; check price!');
 
         _tokenIds.increment();
 
         uint256 newItemId = _tokenIds.current();
-        _mint(recipient, newItemId);
-        _setTokenURI(newItemId, tokenURI);
+        
+        // check if tokens are not up to max supply
+        require(newItemId <= maxSupply);
+        
+        _mint(_recipient, newItemId);
+        _setTokenURI(newItemId, _tokenURI);
 
         return newItemId;
     }
-
-
-    function withdrawEther(uint256 amount) 
-        public 
+    
+    
+    
+    function airDrop(address _recipient, string memory _tokenURI)
+        public onlyOwner
+        returns(uint256)
     {
-        // can only be called by owner
-        require(msg.sender == contractOwner, 'Method reserved for owner');
-        contractOwner.transfer(amount);
+        if (presale)
+        {
+            // check if the address is whitelisted
+            require(isAddressWhitelisted(_recipient), 'Only whitelisted addresses can mint during presale');
+        }
+        
+        require(!paused, 'Minting is paused');
+        ++airdropTokenId;
+        
+        // check if tokens are not up to max supply
+        require(airdropTokenId <= maxSupply);
+        
+        _mint(_recipient, airdropTokenId);
+        _setTokenURI(airdropTokenId, _tokenURI);
+
+        return airdropTokenId;
+    }
+
+
+
+    function withdrawEther(uint256 _amount) 
+        public onlyOwner
+    {
+        payable(msg.sender).transfer(_amount);
     }
     
     
-    function transferEther(address payable addr, uint256 amount) 
-        public 
+    
+    function transferEther(address payable _addr, uint256 _amount) 
+        public onlyOwner
     {
-        // can only be called by owner
-        require(msg.sender == contractOwner, 'Method reserved for owner');
-        addr.transfer(amount);
+        _addr.transfer(_amount);
     }
+    
     
     
     function checkBalance() 
         public view 
+        onlyOwner
         returns(uint256) 
     {
         return(address(this).balance);
     }
+    
+    
+    
+    function setPresale(bool _value)
+        public onlyOwner
+    {
+        presale = _value;
+    }
+    
+    
+    
+    function setCost(uint256 _newCost) 
+        public onlyOwner
+    {
+        cost = _newCost;
+    }
+    
+    
+    
+    function getTotalMinted()
+        public view
+        returns(uint256)
+    {
+        return _tokenIds.current();
+    }
+    
+    
+    
+    function whitelistAddresses(address[] memory _addrs)
+        public onlyOwner
+    {
+        for (uint256 i = 1; i <= _addrs.length; i++)
+        {
+            whitelistedAddresses.push(_addrs[i]);
+        }
+    }
+    
+    
+    
+    function getWhitelistedAddresses()
+        public view
+        onlyOwner
+        returns(address[] memory)
+    {
+        return whitelistedAddresses;
+    }
+    
+    
+    
+    function allWhitelistedAddresses()
+        public view
+        onlyOwner returns(address[] memory)
+    {
+        return whitelistedAddresses;
+    }
+    
+    
+    
+    function isAddressWhitelisted(address _addr)
+        public view
+        onlyOwner returns(bool)
+    {
+        for (uint256 i = 1; i <= whitelistedAddresses.length; i++)
+        {
+            if (whitelistedAddresses[i] == _addr)
+            {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- nft-website/src/utils/Interact.js
+++ nft-website/src/utils/Interact.js
@@ -6,7 +6,7 @@ const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
 const web3 = createAlchemyWeb3(alchemyKey);
 
 const contractABI = require('../contract-abi.json');
-const contractAddress = "0x6Fa5A17012d97e65816d48f9BE8f4E1f4B3B79e7";
+const contractAddress = "0xe0D2009688b97ecdE5E58BEDb8C9370bDAC900F3";
 
 // wallet connection
 export const connectWallet = async () => {
