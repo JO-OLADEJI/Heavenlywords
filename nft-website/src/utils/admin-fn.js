ba69892ff207b0131d8e134db7154c140477d5f1
Joshua Oladeji <dejijolaoluwa@gmail.com>
2021-10-26 16:52:40 +0100

    added admin functions to modify state

---
+++ nft-website/src/utils/admin-fn.js
@@ -0,0 +1,533 @@
+require('dotenv').config();
+const alchemyKey = process.env.REACT_APP_ALCHEMY_URL;
+const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
+const web3 = createAlchemyWeb3(alchemyKey);
+const contractABI = require('../contract-abi.json');
+const contractAddress = "0xf70185778Cb09Ce32a3aa3EC8143e9093c316bA2";
+
+
+
+/**
+ * @param {string} addr - eth wallet address to be whitelisted
+ * @returns {string} - etherscan transaction link || error message
+ */
+export const whitelistAddress = async (addr) => {
+  const heavenlywords = new web3.eth.Contract(contractABI, contractAddress);
+
+  const transactionParameters = {
+    to: contractAddress,
+    from: window.ethereum.selectedAddress,
+    'data': heavenlywords.methods.whitelistAddress(addr).encodeABI(),
+  };
+
+  // -> sign the transaction via Metamask
+  try {
+    const txHash = await window.ethereum.request({
+      method: 'eth_sendTransaction',
+      params: [transactionParameters],
+    });
+
+    return {
+      success: true,
+      status: "✅ Check out your transaction on Etherscan: https://rinkeby.etherscan.io/tx/" + txHash
+    };
+  }
+  catch (error) {
+    return {
+      success: false,
+      status: "😥 Something went wrong: " + error.message
+    };
+  }
+}
+
+
+
+/**
+ * @param {string} addr - eth wallet address to be removed from whitelist
+ * @returns {string} - etherscan transaction link || error message
+ */
+export const removeFromWhitelist = async (addr) => {
+  const heavenlywords = new web3.eth.Contract(contractABI, contractAddress);
+
+  const transactionParameters = {
+    to: contractAddress,
+    from: window.ethereum.selectedAddress,
+    'data': heavenlywords.methods.removeFromWhitelist(addr).encodeABI(),
+  };
+
+  // -> sign the transaction via Metamask
+  try {
+    const txHash = await window.ethereum.request({
+      method: 'eth_sendTransaction',
+      params: [transactionParameters],
+    });
+
+    return {
+      success: true,
+      status: "✅ Check out your transaction on Etherscan: https://rinkeby.etherscan.io/tx/" + txHash
+    };
+  }
+  catch (error) {
+    return {
+      success: false,
+      status: "😥 Something went wrong: " + error.message
+    };
+  }
+}
+
+
+
+/**
+ * @param {Array} IDs - token ids of the URIs
+ * @param {Array} URIs - array of IPFS metadata files of the corresponding token ids
+ * @returns {string} - etherscan transaction link || error message
+ */
+export const updateURIs = async (IDs, URIs) => {
+  const heavenlywords = new web3.eth.Contract(contractABI, contractAddress);
+
+  const transactionParameters = {
+    to: contractAddress,
+    from: window.ethereum.selectedAddress,
+    'data': heavenlywords.methods.updateURIs(IDs, URIs).encodeABI(),
+  };
+
+  // -> sign the transaction via Metamask
+  try {
+    const txHash = await window.ethereum.request({
+      method: 'eth_sendTransaction',
+      params: [transactionParameters],
+    });
+
+    return {
+      success: true,
+      status: "✅ Check out your transaction on Etherscan: https://rinkeby.etherscan.io/tx/" + txHash
+    };
+  }
+  catch (error) {
+    return {
+      success: false,
+      status: "😥 Something went wrong: " + error.message
+    };
+  }
+}
+
+
+
+/**
+ * @param {number} id - token id
+ * @param {string} uri - link of IPFS metadata file of the corresponding token id
+ * @returns {string} - etherscan transaction link || error message
+ */
+export const updateOneURI = async (id, uri) => {
+  const heavenlywords = new web3.eth.Contract(contractABI, contractAddress);
+
+  const transactionParameters = {
+    to: contractAddress,
+    from: window.ethereum.selectedAddress,
+    'data': heavenlywords.methods.updateOneURI(id, uri).encodeABI(),
+  };
+
+  // -> sign the transaction via Metamask
+  try {
+    const txHash = await window.ethereum.request({
+      method: 'eth_sendTransaction',
+      params: [transactionParameters],
+    });
+
+    return {
+      success: true,
+      status: "✅ Check out your transaction on Etherscan: https://rinkeby.etherscan.io/tx/" + txHash
+    };
+  }
+  catch (error) {
+    return {
+      success: false,
+      status: "😥 Something went wrong: " + error.message
+    };
+  }
+}
+
+
+
+/**
+ * @returns {string} - etherscan transaction link || error message
+ */
+export const reveal = async () => {
+  const heavenlywords = new web3.eth.Contract(contractABI, contractAddress);
+
+  const transactionParameters = {
+    to: contractAddress,
+    from: window.ethereum.selectedAddress,
+    'data': heavenlywords.methods.reveal().encodeABI(),
+  };
+
+  // -> sign the transaction via Metamask
+  try {
+    const txHash = await window.ethereum.request({
+      method: 'eth_sendTransaction',
+      params: [transactionParameters],
+    });
+
+    return {
+      success: true,
+      status: "✅ Check out your transaction on Etherscan: https://rinkeby.etherscan.io/tx/" + txHash
+    };
+  }
+  catch (error) {
+    return {
+      success: false,
+      status: "😥 Something went wrong: " + error.message
+    };
+  }
+}
+
+
+
+/**
+ * @param {number} id - token id of nft to be revealed
+ * @returns {string} - etherscan transaction link || error message
+ */
+export const revealOne = async (id) => {
+  const heavenlywords = new web3.eth.Contract(contractABI, contractAddress);
+
+  const transactionParameters = {
+    to: contractAddress,
+    from: window.ethereum.selectedAddress,
+    'data': heavenlywords.methods.revealOne(id).encodeABI(),
+  };
+
+  // -> sign the transaction via Metamask
+  try {
+    const txHash = await window.ethereum.request({
+      method: 'eth_sendTransaction',
+      params: [transactionParameters],
+    });
+
+    return {
+      success: true,
+      status: "✅ Check out your transaction on Etherscan: https://rinkeby.etherscan.io/tx/" + txHash
+    };
+  }
+  catch (error) {
+    return {
+      success: false,
+      status: "😥 Something went wrong: " + error.message
+    };
+  }
+}
+
+
+
+/**
+ * @param {string} newURI - IPFS link to the preview metadata file
+ * @returns {string} - etherscan transaction link || error message
+ */
+export const setInitialURI = async (newURI) => {
+  const heavenlywords = new web3.eth.Contract(contractABI, contractAddress);
+
+  const transactionParameters = {
+    to: contractAddress,
+    from: window.ethereum.selectedAddress,
+    'data': heavenlywords.methods.setInitialURI(newURI).encodeABI(),
+  };
+
+  // -> sign the transaction via Metamask
+  try {
+    const txHash = await window.ethereum.request({
+      method: 'eth_sendTransaction',
+      params: [transactionParameters],
+    });
+
+    return {
+      success: true,
+      status: "✅ Check out your transaction on Etherscan: https://rinkeby.etherscan.io/tx/" + txHash
+    };
+  }
+  catch (error) {
+    return {
+      success: false,
+      status: "😥 Something went wrong: " + error.message
+    };
+  }
+}
+
+
+
+/**
+ * @param {number} value - the new limit on toknes supply
+ * @returns {string} - etherscan transaction link || error message
+ */
+export const setMaxSupply = async (value) => {
+  const heavenlywords = new web3.eth.Contract(contractABI, contractAddress);
+
+  const transactionParameters = {
+    to: contractAddress,
+    from: window.ethereum.selectedAddress,
+    'data': heavenlywords.methods.setMaxSupply(value).encodeABI(),
+  };
+
+  // -> sign the transaction via Metamask
+  try {
+    const txHash = await window.ethereum.request({
+      method: 'eth_sendTransaction',
+      params: [transactionParameters],
+    });
+
+    return {
+      success: true,
+      status: "✅ Check out your transaction on Etherscan: https://rinkeby.etherscan.io/tx/" + txHash
+    };
+  }
+  catch (error) {
+    return {
+      success: false,
+      status: "😥 Something went wrong: " + error.message
+    };
+  }
+}
+
+
+
+/**
+ * @param {boolean} value - the new state of presale
+ * @returns {string} - etherscan transaction link || error message
+ */
+export const setPresale = async (value) => {
+  const heavenlywords = new web3.eth.Contract(contractABI, contractAddress);
+
+  const transactionParameters = {
+    to: contractAddress,
+    from: window.ethereum.selectedAddress,
+    'data': heavenlywords.methods.setPresale(value).encodeABI(),
+  };
+
+  // -> sign the transaction via Metamask
+  try {
+    const txHash = await window.ethereum.request({
+      method: 'eth_sendTransaction',
+      params: [transactionParameters],
+    });
+
+    return {
+      success: true,
+      status: "✅ Check out your transaction on Etherscan: https://rinkeby.etherscan.io/tx/" + txHash
+    };
+  }
+  catch (error) {
+    return {
+      success: false,
+      status: "😥 Something went wrong: " + error.message
+    };
+  }
+}
+
+
+
+/**
+ * @param {boolean} value - the new state of the contract being paused
+ * @returns {string} - etherscan transaction link || error message
+ */
+export const setPaused = async (value) => {
+  const heavenlywords = new web3.eth.Contract(contractABI, contractAddress);
+
+  const transactionParameters = {
+    to: contractAddress,
+    from: window.ethereum.selectedAddress,
+    'data': heavenlywords.methods.setPaused(value).encodeABI(),
+  };
+
+  // -> sign the transaction via Metamask
+  try {
+    const txHash = await window.ethereum.request({
+      method: 'eth_sendTransaction',
+      params: [transactionParameters],
+    });
+
+    return {
+      success: true,
+      status: "✅ Check out your transaction on Etherscan: https://rinkeby.etherscan.io/tx/" + txHash
+    };
+  }
+  catch (error) {
+    return {
+      success: false,
+      status: "😥 Something went wrong: " + error.message
+    };
+  }
+}
+
+
+
+/**
+ * @param {string} newCost - the new cost of the nft in wei (eth * 10^18)
+ * @returns {string} - etherscan transaction link || error message
+ */
+// -> this function can only be called by the admin
+export const setCost = async (newCost) => {
+  const heavenlywords = new web3.eth.Contract(contractABI, contractAddress);
+
+  const transactionParameters = {
+    to: contractAddress,
+    from: window.ethereum.selectedAddress,
+    'data': heavenlywords.methods.setCost(newCost).encodeABI(),
+  };
+
+  // -> sign the transaction via Metamask
+  try {
+    const txHash = await window.ethereum.request({
+      method: 'eth_sendTransaction',
+      params: [transactionParameters],
+    });
+
+    return {
+      success: true,
+      status: "✅ Check out your transaction on Etherscan: https://rinkeby.etherscan.io/tx/" + txHash
+    };
+  }
+  catch (error) {
+    return {
+      success: false,
+      status: "😥 Something went wrong: " + error.message
+    };
+  }
+}
+
+
+
+/**
+ * @param {string} addr - the address to be added as an admin
+ * @returns {string} - etherscan transaction link || error message
+ */
+export const setAdmin = async (addr) => {
+  const heavenlywords = new web3.eth.Contract(contractABI, contractAddress);
+
+  const transactionParameters = {
+    to: contractAddress,
+    from: window.ethereum.selectedAddress,
+    'data': heavenlywords.methods.setAdmin(addr).encodeABI(),
+  };
+
+  // -> sign the transaction via Metamask
+  try {
+    const txHash = await window.ethereum.request({
+      method: 'eth_sendTransaction',
+      params: [transactionParameters],
+    });
+
+    return {
+      success: true,
+      status: "✅ Check out your transaction on Etherscan: https://rinkeby.etherscan.io/tx/" + txHash
+    };
+  }
+  catch (error) {
+    return {
+      success: false,
+      status: "😥 Something went wrong: " + error.message
+    };
+  }
+}
+
+
+
+/**
+ * @param {string} addr - the address to be removed as an admin
+ * @returns {string} - etherscan transaction link || error message
+ */
+export const removeAdmin = async (addr) => {
+  const heavenlywords = new web3.eth.Contract(contractABI, contractAddress);
+
+  const transactionParameters = {
+    to: contractAddress,
+    from: window.ethereum.selectedAddress,
+    'data': heavenlywords.methods.removeAdmin(addr).encodeABI(),
+  };
+
+  // -> sign the transaction via Metamask
+  try {
+    const txHash = await window.ethereum.request({
+      method: 'eth_sendTransaction',
+      params: [transactionParameters],
+    });
+
+    return {
+      success: true,
+      status: "✅ Check out your transaction on Etherscan: https://rinkeby.etherscan.io/tx/" + txHash
+    };
+  }
+  catch (error) {
+    return {
+      success: false,
+      status: "😥 Something went wrong: " + error.message
+    };
+  }
+}
+
+
+
+/**
+ * @param {Array} addrs - list of all addresses to be whitelisted
+ * @returns {string} - etherscan transaction link || error message
+ */
+export const whitelistBatch = async (addrs) => {
+  const heavenlywords = new web3.eth.Contract(contractABI, contractAddress);
+
+  const transactionParameters = {
+    to: contractAddress,
+    from: window.ethereum.selectedAddress,
+    'data': heavenlywords.methods.whitelistBatch(addrs).encodeABI(),
+  };
+
+  // -> sign the transaction via Metamask
+  try {
+    const txHash = await window.ethereum.request({
+      method: 'eth_sendTransaction',
+      params: [transactionParameters],
+    });
+
+    return {
+      success: true,
+      status: "✅ Check out your transaction on Etherscan: https://rinkeby.etherscan.io/tx/" + txHash
+    };
+  }
+  catch (error) {
+    return {
+      success: false,
+      status: "😥 Something went wrong: " + error.message
+    };
+  }
+}
+
+
+
+/**
+ * @param {Array} addrs - list of all addresses to be removed from whitelist
+ * @returns {string} - etherscan transaction link || error message
+ */
+export const removeBatch = async (addrs) => {
+  const heavenlywords = new web3.eth.Contract(contractABI, contractAddress);
+
+  const transactionParameters = {
+    to: contractAddress,
+    from: window.ethereum.selectedAddress,
+    'data': heavenlywords.methods.removeBatch(addrs).encodeABI(),
+  };
+
+  // -> sign the transaction via Metamask
+  try {
+    const txHash = await window.ethereum.request({
+      method: 'eth_sendTransaction',
+      params: [transactionParameters],
+    });
+
+    return {
+      success: true,
+      status: "✅ Check out your transaction on Etherscan: https://rinkeby.etherscan.io/tx/" + txHash
+    };
+  }
+  catch (error) {
+    return {
+      success: false,
+      status: "😥 Something went wrong: " + error.message
+    };
+  }
+}
